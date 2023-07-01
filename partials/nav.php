0e51aefd34ab33e8c35bbee4b9e82b70d74cc69e
TaeshinUshiroda <tu34@njit.edu>
2023-07-01 18:04:13 -0400

    Finish basic registration

--- partials/nav.php
+++ partials/nav.php
@@ -1,11 +1,31 @@
 <?php
-//include functions here so we can have it on every page that uses the nav bar
-//that way we don't need to include so many other files on each page
-//nav will pull in functions and functions will pull in db
+//Resolve cookie issues with port numbers
+$domain = $_SERVER["HTTP_HOST"];
+if (strpos($domain, ":")) {
+   $domain = explode(":", $domain)[0];
+}
+$localWorks = false; //if have issues with localhost for cookie params make false
+
+//extra condition to resolve localhost issue?
+if (($localWorks && $domain == "localhost") || $domain != "localhost") {
+   session_set_cookie_params([
+      "lifetime" => 60 * 60,
+      "path" => "/Project",
+      //"domain" => $_SERVER["HTTP_HOST"] || "localhost",
+      "domain" => $domain,
+      "secure" => true,
+      "httponly" => true,
+      "samesite" => "lax"
+   ]);
+}
+session_start();
+
+//including nav will also include functions, which also includes the other thing
 require(__DIR__ . "/../lib/functions.php");
 ?>
 <nav>
    <ul>
+      <li><a href="home.php">Home</a></li>
       <li><a href="login.php">Login</a></li>
       <li><a href="register.php">Register</a></li>
       <li><a href="logout.php">Logout</a></li>
--- public_html/Project/register.php
+++ public_html/Project/register.php
@@ -1,7 +1,6 @@
 <?php
 require(__DIR__ . "/../../partials/nav.php");
 ?>
-
 <form onsubmit="return validate(this)" method="POST">
    <div>
       <label for="email">Email</label>
@@ -30,52 +29,55 @@ require(__DIR__ . "/../../partials/nav.php");
 if (isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["confirm"])) {
    $email = se($_POST, "email", "", false);
    $password = se($_POST, "password", "", false);
-   $confirm = se($_POST, "confirm", "", false);
-}
-
-//TODO 3:
-$hasError = false;
-if (empty($email)) {
-   echo "Email must not be empty";
-   $hasError = true;
-}
-if (empty($password)) {
-   echo "Password must not be empty";
-   $hasError = true;
-}
-if (empty($confirm)) {
-   echo "Confirm password must not be empty";
-   $hasError = true;
-}
-if (strlen($password) < 8) {
-   echo "Password too short";
-   $hasError = true;
-}
-if (strlen($password) > 0 && $password != $confirm) {
-   echo "Passwords must match";
-   $hasError = true;
-}
-
-//sanitize
-$email = filter_var($email, FILTER_SANITIZE_EMAIL);
-//validate
-if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-   echo "Invalide email address";
-   $hasError = true;
-}
-
-if (!$hasError) {
-   // echo "Welcome, $email";
-   //TODO 4
-   $hash = password_hash($password, PASSWORD_BCRYPT);
-   $db = getDB();
-   $stmt = $db->prepare("INSERT INTO Users(email, password) VALUES (:email, :password)");
-   try {
-      $r = $stmt->execute([":email" => $email, "password" => $hash]);
-      echo "Successfully registered";
-   } catch (Exception $e) {
-      echo "There was an error registering<br>";
-      echo "<pre>" . var_export($e, true) . "</pre>";
+   $confirm = se(
+      $_POST,
+      "confirm",
+      "",
+      false
+   );
+   //TODO 3
+   $hasError = false;
+   if (empty($email)) {
+      echo "Email must not be empty";
+      $hasError = true;
+   }
+   //sanitize
+   $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+   //validate
+   if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      echo "Invalid email address";
+      $hasError = true;
+   }
+   if (empty($password)) {
+      echo "Password must not be empty";
+      $hasError = true;
+   }
+   if (empty($confirm)) {
+      echo "Confirm password must not be empty";
+      $hasError = true;
+   }
+   if (strlen($password) < 8) {
+      echo "Password too short";
+      $hasError = true;
+   }
+   if (
+      strlen($password) > 0 && $password !== $confirm
+   ) {
+      echo "Passwords must match";
+      $hasError = true;
+   }
+   if (!$hasError) {
+      //TODO 4
+      $hash = password_hash($password, PASSWORD_BCRYPT);
+      $db = getDB();
+      $stmt = $db->prepare("INSERT INTO Users (email, password) VALUES(:email, :password)");
+      try {
+         $stmt->execute([":email" => $email, ":password" => $hash]);
+         echo "Successfully registered!";
+      } catch (Exception $e) {
+         echo "There was a problem registering";
+         "<pre>" . var_export($e, true) . "</pre>";
+      }
    }
 }
 ?>
