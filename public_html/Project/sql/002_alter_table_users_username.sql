783c432065b43a944285c56daf21b27de4d97b79
TaeshinUshiroda <tu34@njit.edu>
2023-07-09 20:18:06 -0400

    move project folder

--- public_html/Project/sql/002_alter_table_users_username.sql
+++
@@ -1,5 +0,0 @@
--- Active: 1685589696481@@db.ethereallab.app@3306@tu34
-ALTER TABLE Users
-ADD
-    COLUMN username varchar(30) not null unique default (substring_index(email, '@', 1)) COMMENT 'Username field that defaults to the name of the email given';
-    
