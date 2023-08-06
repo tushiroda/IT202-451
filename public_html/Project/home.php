76aad1f210d86bc94b249cd60130cd42e16a2820
TaeshinUshiroda <tu34@njit.edu>
2023-08-07 00:16:34 +0900

    Show leaderboard on homepage with display options

--- public_html/Project/home.php
+++ public_html/Project/home.php
@@ -1,13 +1,34 @@
+<head>
+   <title>Arcade Homepage</title>
+</head>
+
 <?php
 require(__DIR__ . "/../../partials/nav.php");
+if (isset($_GET['duration'])) {
+   $_SESSION['duration'] = $_GET['duration'];
+}
+require_once(__DIR__ . "/../../partials/scoreTable.php");
+unset($_SESSION['duration']);
 ?>
 
-<h1>Home</h1>
+<br>
+<h1>Arcade Project Homepage</h1>
+
+<div id="description">
+   Welcome to the arcade! This the the homepage for my pong website project. Pong is a game where you score goals by
+   using a paddle to hit a ball past your enemy. To play, click down below!
+</div>
+
+<form action="<?php echo get_url('game.php'); ?>">
+   <button class="mt-3 btn btn-primary" type="submit">Play Pong!</button>
+</form>
+
 <?php
 if (is_logged_in(true)) {
    //comment this out if you don't want to see the session variables
    error_log("Session data: " . var_export($_SESSION, true));
 }
+if (isset($_SESSION['']))
 ?>
 
 <?php
